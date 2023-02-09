ad8c66465331246d190a8d047138424fce95d60e
SIVANESAN S <112241784+SIVANESAN2061@users.noreply.github.com>
2023-02-09 14:55:19 +0530

    Add files via upload

---
+++ 10.binarysearch.C
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int c,first,last,middle,n,search,array[100];
+clrscr();
+printf("enter the number of elements\n");
+scanf("%d",&n);
+printf("enter the %d integers=\n",n);
+for(c=0;c<n;c++)
+scanf("%d",&array[c]);
+printf("enter value to find\n");
+scanf("%d",&search);
+first=0;
+last=n-1;
+middle=(first+last)/2;
+while(first<=last)
+{
+if(array[middle]<search)
+first=middle+1;
+else if(array[middle]==search)
+{
+printf("%d found at location %d\n",search,middle+1);
+break;
+}
+else
+last=middle-1;
+middle=(first+last)/2;
+}
+if(first>last)
+printf("not found!%d isn't present in the list\n",search);
+getch();
+return 0;
+}
